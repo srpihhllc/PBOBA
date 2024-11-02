6a3dc3af293de9ba023c89cc9c2e812c840ed0ab
srpihhllc <srpollardsihhllc@gmail.com>
2024-11-02 12:06:40 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# PlaidBridgeOpenBankingApi/__init__.py
+
+# Initialization code for the package
+
+# Import specific modules or classes
+from .module_name import ClassName
+
+# Initialize variables or settings
+__version__ = '0.1.0'
+
+# Any other setup code
